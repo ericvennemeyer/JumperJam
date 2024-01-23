0ffe4bfced395db4faa5047878238501f20ce844
Eric Vennemeyer <82679637+ericvennemeyer@users.noreply.github.com>
2024-01-23 11:50:56 -0800

    Add horizontal player movement

---
+++ scripts/player.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+var speed = 300.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _physics_process(delta):
+	var direction = Input.get_axis("move_left", "move_right")
+	if direction:
+		velocity.x = direction * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		
+	move_and_slide()

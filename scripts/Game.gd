f050e4209dcbad8c42decc6a3e90a3362f4ca3eb
Eric Vennemeyer <82679637+ericvennemeyer@users.noreply.github.com>
2024-01-26 10:56:19 -0800

    Add game camera

--- scripts/Game.gd
+++ scripts/Game.gd
@@ -1,11 +1,12 @@
 extends Node2D
 
+var camera_scene = preload("res://scenes/game_camera.tscn")
+var camera = null
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	camera = camera_scene.instantiate()
+	camera.setup_camera($Player)
+	add_child(camera)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass

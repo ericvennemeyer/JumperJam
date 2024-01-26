d30845d1d4b92a8b23e85dea8776c1a206d92b7f
Eric Vennemeyer <82679637+ericvennemeyer@users.noreply.github.com>
2024-01-26 11:14:12 -0800

    Add camera limits

--- scripts/game_camera.gd
+++ scripts/game_camera.gd
@@ -1,12 +1,20 @@
 extends Camera2D
 
 var player: Player = null
+var viewport_size
 
 func _ready():
-	global_position.x = get_viewport_rect().size.x / 2
+	viewport_size = get_viewport_rect().size
+	global_position.x = viewport_size.x / 2
+	
+	limit_bottom = viewport_size.y
+	limit_left = 0
+	limit_right = viewport_size.x
 
 func _process(delta):
-	pass
+	if player:
+		if limit_bottom > player.global_position.y + (viewport_size.y / 2):
+			limit_bottom = player.global_position.y + (viewport_size.y / 2)
 
 func _physics_process(delta):
 	if player:

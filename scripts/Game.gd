32f50b55028f493c602ddfeef85942cee13a470d
Eric Vennemeyer <82679637+ericvennemeyer@users.noreply.github.com>
2024-01-29 10:45:40 -0800

    Add programmatic ground platform generation

--- scripts/Game.gd
+++ scripts/Game.gd
@@ -12,7 +12,15 @@ func _ready():
 	camera.setup_camera($Player)
 	add_child(camera)
 	
-	create_platform(Vector2(100, 300))
+	# Generate ground
+	var viewport_size = get_viewport_rect().size
+	var platform_width = 137
+	var ground_layer_platform_count = (viewport_size.x / platform_width) + 1
+	var ground_layer_y_offset = 62
+	
+	for i in range(ground_layer_platform_count):
+		var ground_location = (Vector2(i * platform_width, viewport_size.y - ground_layer_y_offset))
+		create_platform(ground_location)
 
 func _process(delta):
 	if Input.is_action_just_pressed("quit"):

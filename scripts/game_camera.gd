c5c22800a212c01b8bdc0f0b6be342364d80643d
Eric Vennemeyer <82679637+ericvennemeyer@users.noreply.github.com>
2024-01-30 10:21:00 -0800

    Add platform destroyer

--- scripts/game_camera.gd
+++ scripts/game_camera.gd
@@ -1,5 +1,8 @@
 extends Camera2D
 
+@onready var destroyer = $Destroyer
+@onready var destroyer_shape = $Destroyer/CollisionShape2D
+
 var player: Player = null
 var viewport_size
 
@@ -10,11 +13,24 @@ func _ready():
 	limit_bottom = viewport_size.y
 	limit_left = 0
 	limit_right = viewport_size.x
+	
+	destroyer.position.y = viewport_size.y
+	
+	var rect_shape = RectangleShape2D.new()
+	var rect_shape_size = Vector2(viewport_size.x, 200)
+	rect_shape.set_size(rect_shape_size)
+	destroyer_shape.shape = rect_shape
 
 func _process(delta):
 	if player:
 		if limit_bottom > player.global_position.y + (viewport_size.y / 2):
 			limit_bottom = player.global_position.y + (viewport_size.y / 2)
+	
+	var overlapping_areas = destroyer.get_overlapping_areas()
+	if overlapping_areas.size() > 0:
+		for area in overlapping_areas:
+			if area is Platform:
+				area.queue_free()
 
 func _physics_process(delta):
 	if player:
--- scripts/platform.gd
+++ scripts/platform.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+class_name Platform
+
 func _on_body_entered(body):
 	if body is Player:
 		if body.velocity.y > 0:

2ea8927aaecf74145e635db3a1a9ce71a9446fbb
Eric Vennemeyer <82679637+ericvennemeyer@users.noreply.github.com>
2024-01-30 10:39:24 -0800

    Refactor game init code in Game.gd and spawn player from script

--- scripts/Game.gd
+++ scripts/Game.gd
@@ -1,22 +1,36 @@
 extends Node2D
 
-@onready var player = $Player
 @onready var level_generator = $LevelGenerator
 
 var camera_scene = preload("res://scenes/game_camera.tscn")
+var player_scene = preload("res://scenes/player.tscn")
+var player_spawn_pos: Vector2
+var player_spawn_pos_y_offset = 135
 
 var camera = null
+var player: Player = null
 
 func _ready():
-	camera = camera_scene.instantiate()
-	camera.setup_camera($Player)
-	add_child(camera)
+	var viewport_size = get_viewport_rect().size
+	player_spawn_pos.x = viewport_size.x / 2.0
+	player_spawn_pos.y = viewport_size.y - player_spawn_pos_y_offset
 	
-	if player:
-		level_generator.setup(player)
+	new_game()
 
 func _process(delta):
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
 	if Input.is_action_just_pressed("reset"):
 		get_tree().reload_current_scene()
+
+func new_game():
+	player = player_scene.instantiate()
+	player.global_position = player_spawn_pos
+	add_child(player)
+	
+	camera = camera_scene.instantiate()
+	camera.setup_camera(player)
+	add_child(camera)
+	
+	if player:
+		level_generator.setup(player)
